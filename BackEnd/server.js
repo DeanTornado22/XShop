f35d237b8aff8fd34532cf48b3700106ccc58356
DeanTornado22 <deantornadoo@gmail.com>
2021-05-04 02:23:40 +0700

    Prepare for deployment

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,6 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import path from 'path'
 
 dotenv.config()
 
@@ -20,13 +21,22 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/FrontEnd/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'FrontEnd', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 app.use(notFound)
 
 app.use(errorHandler)
